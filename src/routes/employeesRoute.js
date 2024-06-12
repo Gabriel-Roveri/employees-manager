b38f3c5105679a7a6ac178dac73de6bce01faa5d
Gabriel Roveri <groveri.gr@outlook.com>
2024-06-12 20:30:43 -0300

    Adding CRUD methods and sync it

--- src/routes/employeesRoute.js
+++ src/routes/employeesRoute.js
@@ -5,6 +5,11 @@ const employeesController = new EmployeesController();
 
 const router = Router();
 
-router.get('/employees',  (req, res) => employeesController.getAll(req,res));
+router
+    .get('/employees', (req, res) => employeesController.getAll(req, res))
+    .get('/employees/:id', (req, res) => employeesController.getById(req, res))
+    .post('/employees', (req, res) => employeesController.newLog(req, res))
+    .put('/employees:id', (req, res) => employeesController.update(req, res))
+    .delete('/employees:id',  (req, res) => employeesController.del(req,res));
 
 module.exports = router;
--- src/services/Services.js
+++ src/services/Services.js
@@ -8,6 +8,31 @@ class Services {
     async getAllLogs() {
         return dataSource[this.model].findAll();
     }
+
+    async getLogById(id) {
+        return dataSource[this.model].findByPk(id);
+    }
+
+    async createLog(logData) {
+        return dataSource[this.model].create(logData);
+    }
+    
+    async logUpdate(updatedData, id) {
+
+        const updatedLogList = dataSource[this.model].update(updatedData, {
+            where: { id: id }
+        });
+
+        if (updatedLogList[0] === 0) {
+            return false;
+        }
+        
+        return true;
+    }
+
+    async delLog(id) {
+        return dataSource[this.model].destroy({ where: { id: id } });
+    }
 }
 
 module.exports = Services;
